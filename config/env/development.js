af0a6d31c50222a7258e612c2c604354c7998be1
kingasawa <trancatkhanh@gmail.com>
2017-09-26 19:21:09 +0700

    connect to shopify store

--- config/env/development.js
+++ config/env/development.js
@@ -49,8 +49,8 @@ module.exports = {
   models: {
     migrate: 'alter',
 
-    connection: 'localhost',
-    // connection: 'production',
+    // connection: 'localhost',
+    connection: 'production',
 
   },
 
